4fa0df94a33a5f7bdcbd58253dda7b6843c47790
heowan <101536766+heowan@users.noreply.github.com>
2023-09-06 12:58:22 +0900

    feat: 검색창 ui 구현

--- src/pages/MainPage/Main.tsx
+++ src/pages/MainPage/Main.tsx
@@ -1,5 +1,15 @@
+import styles from './Main.module.scss';
+
 function Main() {
-	return <div> 메인화면입니다. </div>;
+	return (
+		<div className={styles.container}>
+			<div className={styles.searchBar}>
+				<input className={styles.input} type="text" placeholder="질환명을 입력해주세요." />
+				<button className={styles.btn}>검색</button>
+			</div>
+			<div>추천검색어</div>
+		</div>
+	);
 }
 
 export default Main;
