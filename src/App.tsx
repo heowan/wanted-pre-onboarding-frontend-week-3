240d9f6bb610633cd683cfb38c3e671bab28fa3a
heowan <101536766+heowan@users.noreply.github.com>
2023-09-06 12:07:33 +0900

    feat: 라우터 설정 및 Loading, NotFound 페이지 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Suspense } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { ROUTES } from './utils/constants/constants';
+import Main from './pages/MainPage/Main';
+import NotFound from './pages/NotFoundPage/NotFound';
+import LoadingSpinner from './components/loadingSpinner/LoadingSpinner';
 
 function App() {
 	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.tsx</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-			</header>
-		</div>
+		<>
+			<Suspense fallback={<LoadingSpinner />}>
+				<Routes>
+					<Route path={ROUTES.MAIN} element={<Main />} />
+					<Route path={ROUTES.NOTFOUND} element={<NotFound />} />
+				</Routes>
+			</Suspense>
+		</>
 	);
 }
 
