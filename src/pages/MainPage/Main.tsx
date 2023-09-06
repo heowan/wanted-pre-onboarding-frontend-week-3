d18775b81b040af7f78272225ec1998c57415436
heowan <101536766+heowan@users.noreply.github.com>
2023-09-06 14:39:54 +0900

    feat: useInput 커스텀 훅 생성 및 타이핑 유무에 따른 검색어추천 창 표시 기능

--- src/pages/MainPage/Main.tsx
+++ src/pages/MainPage/Main.tsx
@@ -1,14 +1,24 @@
 import styles from './Main.module.scss';
 import RecommendationContainer from '../../components/recommendation/RecommendationContainer';
+import useInput from '../../hooks/useInput';
 
 function Main() {
+	console.log('렌더링');
+	const [input, inputValueChange] = useInput('');
+
 	return (
 		<div className={styles.container}>
 			<div className={styles.searchBar}>
-				<input className={styles.input} type="text" placeholder="질환명을 입력해주세요." />
+				<input
+					className={styles.input}
+					type="text"
+					placeholder="질환명을 입력해주세요."
+					onChange={inputValueChange}
+					value={input}
+				/>
 				<button className={styles.btn}>검색</button>
 			</div>
-			<RecommendationContainer />
+			{input !== '' ? <RecommendationContainer /> : undefined}
 		</div>
 	);
 }
